42f5c953c49ce9ea0347de7f1f08c54e1c439dd4
Hip168 <DAOLM.B24CC052@stu.ptit.edu.vn>
2025-06-07 12:51:48 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function openTab(evt, tabId) {
-    document.querySelectorAll('.tab-content').forEach(t => t.classList.remove('active'));
-    document.querySelectorAll('.tab-button').forEach(b => b.classList.remove('active'));
-    document.getElementById(tabId).classList.add('active');
-    evt.currentTarget.classList.add('active');
-}
-document.addEventListener('DOMContentLoaded', () => {
-    openTab(null, 'khachhang');
-});
